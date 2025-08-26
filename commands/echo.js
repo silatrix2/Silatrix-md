ab4ae726fdd52768caeef44985535f1f55fdff5e
silatrix2 <silatrix22@gmail.com>
2025-08-26 11:20:27 +0300

    Delete commands directory

--- commands/echo.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'echo',
-  usage: '!echo <ujumbe>',
-  description: 'Bot hurudia ulichoandika.',
-  async run({ sock, msg, text }) {
-    const content = text.trim();
-    await sock.sendMessage(msg.key.remoteJid, { text: content || 'Andika kitu: !echo Mimi ni bingwa' }, { quoted: msg });
-  }
-}
