cb9e73057a8f66fc2ef2c9120f58cc751bbec1a4
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2025-09-09 15:42:51 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,7 +4,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "RAHEEM-XMD-2>>>PoZXHABR#avdbUBpb_iJdpAgpgxTif2tKWE4y30tADqir93bdUqQ",
+  SESSION_ID: process.env.SESSION_ID || "RAHEEM-XMD-2>>>OkB2jZYK#2Br21sLR0ozVrl4SuKGHNEPuUzWW8ZWBrK8Q-wWJ1i4",
   PREFIX: process.env.PREFIX || '+',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
